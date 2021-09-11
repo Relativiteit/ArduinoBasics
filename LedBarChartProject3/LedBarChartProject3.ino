1ce4fe356a6a407697872df905f097beee411931
Relativiteit <alejocain@gmail.com>
2021-09-11 17:46:41 +0200

    fixed bugs

--- LedBarChartProject3/LedBarChartProject3.ino
+++ LedBarChartProject3/LedBarChartProject3.ino
@@ -1,12 +1,12 @@
-/* @author Alejo Cain 
- *  Date: 05-09-2021
- *  Time: 13:10 
+ /* @author Alejo Cain 
+ *  Date: 11-09-2021
+ *  Time: 17:10 
  *  Title: LED Bar Chart 
  */
 
  const int analogPin = A0; // Pin connected to potentiometer
  const int ledCount = 9; // Amounts of LEDS
- int ledPins[] = {2,3,4,5,6,7,8,9}; // Pins connected to the leds 
+ int ledPins[] = {2,3,4,5,6,7,8,9,10}; // Pins connected to the leds 
 
  
 void setup() {
@@ -19,10 +19,10 @@ for (int thisLed = 0; thisLed < ledCount; thisLed++){
 void loop() {
   // put your main code here, to run repeatedly:
 int sensorReading = analogRead(analogPin); //analog input
-int ledLevel = map(sensorReading, 0, 1023, 0, ledCount); // map(value, fromLow, fromHigh, toLow, toHigh)
-for (int thisLed=0; ledCount; thisLed++){
-  if(thisLed < ledLevel)
-  { digitalWrite(ledPins[thisLed], HIGH);  // turn LED's on in order
+int ledLevel = map(sensorReading, 0, 1023, 0, ledCount);// map(value, fromLow, fromHigh, toLow, toHigh)
+for (int thisLed = 0; thisLed < ledCount; thisLed++){
+  if(thisLed < ledLevel){
+    digitalWrite(ledPins[thisLed], HIGH);  // turn LED's on in order
 }
 else { // Turn LED's off in order 
   digitalWrite(ledPins[thisLed], LOW);
